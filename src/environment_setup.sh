429e5920622e413ca8b2f2605692ef956ae8aaa8
Un1XX388 <un1xx388@gmail.com>
2023-03-20 21:35:43 -0600

    Fixed issue in environent_setup that I introduced in the last commit.

--- src/environment_setup.sh
+++ src/environment_setup.sh
@@ -6,8 +6,6 @@
 declare -r BACK_UP_DIR="$PWD/$USER-homeBackUp-$(date +"%Y_%m_%d-%H_%M_%S")"
 # List of users, specificly there UIDs, that script should be run as.
 declare -ra INVALID_USERS="0 65534"
-# TODO: Comment.
-declare -r LOG=$PWD/shell/functions/log.sh -c="environment_setup.sh"
 
 # TODO: Set a default answer and make user input checking more robust.
 # Ask user if the user that's currently running this script is the one they would like to setup.
@@ -42,7 +40,7 @@ if ! [[ " ${INVALID_USERS[*]} " =~ " $EUID " ]]; then
 			
 			# Ensure shell name was found.
 			if [[ "$SHELL" != "$shellName" && "$shellName" != "" ]]; then
-				$LOG -i -m="Creating local directory for storing $USER's file(s) that are being replaced..."
+				printf "Creating local directory for storing $USER's file(s) that are being replaced...\n"
 				
 				declare cmd="mkdir $BACK_UP_DIR"
 				unset stdOut errOut rtOut
