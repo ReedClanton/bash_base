015245b18f0c8fb61a2c13f60706a125c56d5a6e
Un1XX388 <un1xx388@gmail.com>
2023-03-20 19:25:20 -0600

    Replaced usage of bash with shell in the name of a bunch of directories and files. Totally revamped the environment_setup.sh script. Still need to remove TODOs from it, fix some instances of missing new lines, and do some general clean up.

--- src/environment_setup.sh
+++ src/environment_setup.sh
@@ -1,61 +1,252 @@
 #! /bin/bash
 
+# TODO: Add doc.
+
 # Tracks directory that file(s)/directory(ies) being replaced will be moved to.
-declare -r BACK_UP_DIR="$PWD/userHomeBackUp-$(date +"%Y_%m_%d-%H_%M_%S")"
+declare -r BACK_UP_DIR="$PWD/$USER-homeBackUp-$(date +"%Y_%m_%d-%H_%M_%S")"
 # List of users, specificly there UIDs, that script should be run as.
 declare -ra INVALID_USERS="0 65534"
 
-# Attempt to make sure script is being run as a real user.
-if [[ " ${INVALID_USERS[*]} " =~ " $EUID " ]]; then
-	printf "Script should be run using your user, not '$USER'.\n"
+# TODO: Set a default answer and make user input checking more robust.
+# Ask user if the user that's currently running this script is the one they would like to setup.
+printf "The user that's running this script ($USER) is the one that will have there environment setup./n"
+printf "Would you like to run environment setup for '$USER' (y or n): "
+read userAnswer
+if [[ "$userAnswer" != "y" ]]; then
 	exit 21
-else
-	# Ensure script is being run from the same location as it's located.
-	if [[ "./$(basename $0)" != $0 ]]; then
-		printf "Script must be run from the same directory it's located in.\n"
-		exit 22
-	else
-		printf "Script will be removing important environment setup files.\n"
-		printf "Prior to removal they'll be backed up to '$BACK_UP_DIR'.\n"
-		
-		# Check if directory for backing up user's home directory files already exists.
-		if [[ -d "$BACK_UP_DIR" ]]; then
-			printf "Directory used to back up file(s) modified by this script already exists.\n"
-			exit 23
-		else
-			printf "Backing up bash file(s) from $USER's home directory ($HOME)...\n"
-			# TODO: Check if mkdir command worked, if not, exit.
-			mkdir $BACK_UP_DIR
-			# TODO: Check if move was successful, if it's not, exit.
-			mv $HOME/.bash* $BACK_UP_DIR
-			
-			printf "Backing up bash directory(ies) from $USER's home directory ($HOME)...\n"
-			# TODO: Check if move was successful, if it's not, log it.
-			mv $HOME/bash $BACK_UP_DIR
-			
-			printf "Copying file(s) to '$HOME'...\n"
-			cp $PWD/.* $HOME/
-			
-			printf "Copying directory(ies) to '$HOME'...\n"
-			if [[ -d "$HOME/bash" ]]; then
-				printf "Directory named 'bash' already exists in '$HOME'...\n"
-			else
-				cp -r $PWD/bash $HOME/bash
-				printf "Success!\n"
-				exit 0
-			fi
-		fi
-		
-		printf "Something went wrong!\n"
-		printf "Removing file(s)/directory(ies) created by this script from $USER's home directory ($HOME)...\n"
-		# TODO: Ensure that the bellow files were created prior to this script running before removing them.
-		rm $HOME/.bashrc
-		rm $HOME/.bash_profile
-		rm -r $HOME/bash
+fi
 
-		printf "Restoring $USER's original bash file(s)...\n"
-		mv $BACK_UP_DIR/* $HOME/
-		exit 24
+# TODO: Clean up exit values.
+
+# Ensure script is being run by a plausibly valid user.
+if ! [[ " ${INVALID_USERS[*]} " =~ " $EUID " ]]; then
+	# Ensure script is being run from the same location as it's located.
+	if [[ "./$(basename $0)" == $0 ]]; then
+		# Ensure directory for backing up user's environment config doesn't already exist.
+		if [[ ! -d "$BACK_UP_DIR" ]]; then
+			# Determine name of current shell.
+			declare shellName=${SHELL#/*/*/*/*/}
+			if [[ "$SHELL" = "$shellName" ]]; then
+				shellName=${SHELL#/*/*/*/}
+				if [[ "$SHELL" = "$shellName" ]]; then
+					shellName=${SHELL#/*/*/}
+					if [[ "$SHELL" = "$shellName" ]]; then
+						shellName=${SHELL#/*/}
+						if [[ "$SHELL" = "$shellName" ]]; then
+							shellName=${SHELL#/}
+						fi
+					fi
+				fi
+			fi
+			
+			# Ensure shell name was found.
+			if [[ "$SHELL" != "$shellName" && "$shellName" != "" ]]; then
+				printf "Creating local directory for storring $USER's file(s) that are being replaced...\n"
+				
+				declare cmd="mkdir $BACK_UP_DIR"
+				unset stdOut errOut rtOut
+				eval "$( (eval $cmd) \
+					2> >(errOut=$(cat); typeset -p errOut) \
+					 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+			
+				# Ensure directory for storring user's current environment setup was created.
+				if [[ $rtOut -eq 0 ]]; then
+					# Back up user's hidden shell file(s) if they exist.
+					# TODO: Revert bellow home path and remove this comment.
+					if compgen -G "$HOME/setupTest/.$shellName*" > /dev/null; then
+						printf "Backing up '$HOME/.$shellName*' files to '$BACK_UP_DIR'...\n"
+						# TODO: Revert bellow home path and remove this comment.
+						declare cmd="mv $HOME/setupTest/.$shellName* $BACK_UP_DIR/"
+						unset stdOut errOut rtOut
+						eval "$( (eval $cmd) \
+							2> >(errOut=$(cat); typeset -p errOut) \
+							 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+					else
+						printf "No hidden shell file(s) to back up, skipping.\n"
+						rtOut=0
+					fi
+					
+					# Ensure hidden bash file(s) were moved to the back up directory.
+					if [[ $rtOut -eq 0 ]]; then
+						# Directories with the same name as the user's shell must be backed up because they'll be repalced by this script.
+						# TODO: Revert bellow home path and remove this comment.
+						if [[ -d "$HOME/setupTest/$shellName" ]]; then
+							printf "Backing up shell directory '$HOME/$shellName' from $USER's home directory...\n"
+							# TODO: Revert bellow home path and remove this comment.
+							declare cmd="mv $HOME/setupTest/$shellName $BACK_UP_DIR/"
+							unset stdOut errOut rtOut
+							eval "$( (eval $cmd) \
+								2> >(errOut=$(cat); typeset -p errOut) \
+								 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+						else
+							printf "No directory containing environment setup found in $USER's home, back up skipped.\n"
+							rtOut=0
+						fi
+						
+						# Ensure move worked.
+						if [[ $rtOut -eq 0 ]]; then
+							printf "Copying hidden shell setup file(s) from '$PWD/.shell*' file(s) to '$HOME/'...\n"
+							
+							for shellFilePath in $PWD/.shell*; do
+								# TODO: Revert bellow home path and remove this comment.
+								newShellFilePath=$HOME/setupTest/.$shellName${shellFilePath#$PWD/.shell}
+								printf "Copying '$shellFilePath' to '$newShellFilePath'...\n"
+								declare cmd="cp $shellFilePath $newShellFilePath"
+								unset stdOut errOut rtOut
+								eval "$( (eval $cmd) \
+									2> >(errOut=$(cat); typeset -p errOut) \
+									 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+								# Stop copying files if a copy fails.
+								if [[ $rtOut -ne 0 ]]; then
+									printf "Failed to move '$shellFilePath' to '$newShellFilePath'.\n"
+									break
+								fi
+							done
+							
+							# Ensure hidden shell file(s) were moved.
+							if [[ $rtOut -eq 0 ]]; then
+								printf "Copying '$PWD/$shellName' to '$HOME/'...\n"
+								# TODO: Revert bellow home path and remove this comment.
+								declare cmd="cp -r $PWD/shell $HOME/setupTest/$shellName"
+								unset stdOut errOut rtOut
+								eval "$( (eval $cmd) \
+									2> >(errOut=$(cat); typeset -p errOut) \
+									 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+								# Ensure shell directory copy worked.
+								if [[ $rtOut -eq 0 ]]; then
+									printf "Success!\n"
+									exit 0
+								else
+									printf "Failed to copy directory with shell scripts to $USER's home.\n"
+								fi
+								
+								# Remove shell directory that copy failed for.
+								# TODO: Revert bellow home path and remove this comment.
+								if [[ -d "$HOME/setupTest/$shellName" ]]; then
+									printf "Removing '$HOME/$shellName...\n"
+									# TODO: Revert bellow home path and remove this comment.
+									declare cmd="rm -rf $HOME/setupTest/$shellName"
+									unset stdOut errOut rtOut
+									eval "$( (eval $cmd) \
+										2> >(errOut=$(cat); typeset -p errOut) \
+										 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+									# Check if deletion worked.
+									if [[ $rtOut -ne 0 ]]; then
+										printf "Failed to revert environment, you'll need to manually copy contents of '$BACK_UP_DIR' back to '$HOME'.\n"
+										exit 28
+									fi
+								else
+									printf "Skipping removal of '$shellName' directory from $USER's home because it doesn't exist.\n"
+								fi
+								
+								# Check if back up contains a shell directory.
+								if [[ -d "$BACK_UP_DIR/$shellName" ]]; then
+									printf "Copying '$shellName' directory to $USER's home from back up copy...\n"
+									# Copy old shell directory back.
+									# TODO: Revert bellow home path and remove this comment.
+									declare cmd="cp -r $BACK_UP_DIR/$shellName $HOME/setupTest/"
+									unset stdOut errOut rtOut
+									eval "$( (eval $cmd) \
+										2> >(errOut=$(cat); typeset -p errOut) \
+										 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+									# Check if deletion worked.
+									if [[ $rtOut -ne 0 ]]; then
+										printf "Failed to revert environment, you'll need to manually copy contents of '$BACK_UP_DIR' back to '$HOME'.\n"
+										exit 29
+									fi
+								else
+									printf "Skipping copying '$shellName' directory from back up because the back up doesn't contain it.\n"
+								fi
+							else
+								printf "Failed to move hidden shell file(s) to $USER's home.\n"
+							fi
+							
+							# Remove hidden file(s) that were copied to user's home.
+							# TODO: Revert bellow home path and remove this comment.
+							declare cmd="rm $HOME/setupTest/.$shellName*"
+							unset stdOut errOut rtOut
+							eval "$( (eval $cmd) \
+								2> >(errOut=$(cat); typeset -p errOut) \
+								 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+							# Stop copying files if a copy fails.
+							if [[ $rtOut -ne 0 ]]; then
+								printf "Failed to move '$shellFilePath' to '$newShellFilePath'.\n"
+								exit 30
+							fi
+							
+							# TODO: Comment.
+							if compgen -G "$BACK_UP_DIR/.*" > /dev/null; then
+								printf "Copying hidden shell file(s) from back up to $USER's home...\n"
+								# Copy backed up files to user's home.
+								# TODO: Revert bellow home path and remove this comment.
+								declare cmd="cp $BACK_UP_DIR/.* $HOME/setupTest/"
+								unset stdOut errOut rtOut
+								eval "$( (eval $cmd) \
+									2> >(errOut=$(cat); typeset -p errOut) \
+									 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+								# Stop copying files if a copy fails.
+								if [[ $rtOut -ne 0 ]]; then
+									printf "Failed to move hidden files from '$BACK_UP_DIR' back to $USER's home, you'll need to manually copy contents.\n"
+									exit 31
+								fi
+							else
+								printf "Skipping copying of hidden shell file(s) because the back up doesn't contain any.\n"
+							fi
+
+						else
+							printf "Failed to back up (move) '$HOME/$shellName' to '$BACK_UP_DIR/'.\n"
+						fi
+						
+						# Attempt to move shell directory back to user's home.
+						if [[ -d "$BACK_UP_DIR/$shellName" ]]; then
+							# TODO: Revert bellow home path and remove this comment.
+							declare cmd="cp -r $BACK_UP_DIR/$shellName $HOME/setupTest/"
+							unset stdOut errOut rtOut
+							eval "$( (eval $cmd) \
+								2> >(errOut=$(cat); typeset -p errOut) \
+								 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+							# Check if deletion worked.
+							if [[ $rtOut -ne 0 ]]; then
+								printf "Failed to revert environment, you'll need to manually copy contents of '$BACK_UP_DIR' back to '$HOME'.\n"
+								exit 28
+							fi
+						fi
+					else
+						printf "Failed to back up (move) '$HOME/.$shellName*' file(s) to '$BACK_UP_DIR'.\n"
+					fi
+					
+					# Ensure any file(s) that were moved from user's home are moved back.
+					if compgen -G "$BACK_UP_DIR/.$shellName*" > /dev/null; then
+						# TODO: Revert bellow home path and remove this comment.
+						declare cmd="cp $BACK_UP_DIR/.* $HOME/setupTest/"
+						unset stdOut errOut rtOut
+						eval "$( (eval $cmd) \
+							2> >(errOut=$(cat); typeset -p errOut) \
+							 > >(stdOut=$(cat); typeset -p stdOut); rtOut=$?; typeset -p rtOut )"
+						# Check if deletion worked.
+						if [[ $rtOut -ne 0 ]]; then
+							printf "Failed to revert environment, you'll need to manually copy contents of '$BACK_UP_DIR/' back to '$HOME/'.\n"
+							exit 27
+						fi
+					fi
+				else
+					printf "Failed to create directory for backing up $USER's data.\n"
+					exit 26
+				fi
+			else
+				printf "Failed, couldn't determine shell name from \$SHELL ($SHELL).\n"
+				exit 25
+			fi
+		else
+			printf "Failed because back up directory '$BACK_UP_DIR' already exists.\n"
+			exit 24
+		fi
+	else
+		printf "Failed, script must be run from the same directory it's located in.\n"
+		exit 23
 	fi
+else
+	printf "Failed, script must be run using your user, not '$USER'.\n"
+	exit 22
 fi
 
