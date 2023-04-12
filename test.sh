3aa09413d3009878ed1b3ad81452d9792c16c8f4
ReedClanton <clantonreed@gmail.com>
2023-04-12 16:41:09 -0600

    same

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-shellspec --fail-no-examples --warning-as-failure ./spec/unit/output/util/constants_*
+shellspec -s /usr/bin/zsh --fail-no-examples --warning-as-failure ./spec/unit/output/util/constants_*
